128dca2eb540364d2721d8c308f0ee3eb4f98cd4
badl0ck <semenvad1@gmail.com>
2020-10-25 10:00:38 +0800

    all files

---
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main(int argc, char *argv[])
+{
+    WSADATA wsa;
+
+    printf("Initializing WinSock2...\n");
+
+
+    return 0;
+}
