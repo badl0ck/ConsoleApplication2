12a1a5b59d0e166e4802d0fa6369342957b6a2df
badl0ck <semenvad1@gmail.com>
2020-10-25 10:26:52 +0800

    Update ConsoleApplication2.c
    socket created

--- ConsoleApplication2/ConsoleApplication2.c
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[])
 {
     WSADATA wsa;
+    SOCKET s;
 
     printf("Initializing WinSock2...\n");
 
@@ -16,6 +17,14 @@ int main(int argc, char *argv[])
     }
 
     printf("Initialized\n");
+
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
+        printf("Could not create a socket. Error: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    printf("Socket Created\n");
 
     return 0;
 }
