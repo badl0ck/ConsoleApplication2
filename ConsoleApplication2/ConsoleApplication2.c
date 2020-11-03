70b19121e17065e29508ac7197e0a273eee911ce
badl0ck <semenvad1@gmail.com>
2020-11-03 13:18:30 +0800

    Update ConsoleApplication2.c
    connect()

--- ConsoleApplication2/ConsoleApplication2.c
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -1,4 +1,6 @@
-﻿#include <stdio.h>
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
 #include <WinSock2.h>
 
 #pragma comment(lib, "ws2_32.lib")
@@ -7,6 +9,7 @@ int main(int argc, char *argv[])
 {
     WSADATA wsa;
     SOCKET s;
+    struct sockaddr_in server;
 
     printf("Initializing WinSock2...\n");
 
@@ -25,6 +28,18 @@ int main(int argc, char *argv[])
     }
 
     printf("Socket Created\n");
+
+    server.sin_addr.s_addr = inet_addr("108.177.14.138");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+       
+    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        printf("connect error");
+        return 1;
+    }
+
+    printf("connected");
 
     return 0;
 }
